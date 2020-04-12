21fdecabd69ee17d4ec2be478f99c83ff540cc39
Adam Šuba <asuba512@gmail.com>
2020-04-12 17:17:38 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -31,6 +31,8 @@ const theme = createMuiTheme({
         tonalOffset: 0.2,
     },
 })
+console.log(process.env.REACT_APP_API_URL)
+console.log(process.env.NODE_ENV)
 
 ReactDOM.render(
     <MuiPickersUtilsProvider utils={MomentUtils}>
